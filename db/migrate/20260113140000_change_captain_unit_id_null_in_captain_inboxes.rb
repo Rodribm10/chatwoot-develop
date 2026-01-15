2a87ee68b188e9f3ee009082185f06f18f726b97
Rodrigo Borba <rodrigobm10@me.com>
2026-01-15 08:17:08 -0300

    migration quebrada , ajuste feito

--- db/migrate/20260113140000_change_captain_unit_id_null_in_captain_inboxes.rb
+++
@@ -1,5 +0,0 @@
-class ChangeCaptainUnitIdNullInCaptainInboxes < ActiveRecord::Migration[7.0]
-  def change
-    change_column_null :captain_inboxes, :captain_unit_id, true
-  end
-end
--- db/migrate/20260114101010_add_unit_to_captain_inboxes.rb
+++ db/migrate/20260114101010_add_unit_to_captain_inboxes.rb
@@ -1,5 +1,5 @@
 class AddUnitToCaptainInboxes < ActiveRecord::Migration[7.1]
   def change
-    add_reference :captain_inboxes, :captain_unit, null: false, foreign_key: true
+    add_reference :captain_inboxes, :captain_unit, null: true, foreign_key: true
   end
 end
